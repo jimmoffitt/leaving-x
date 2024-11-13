ff079bb28475fec20f9c3f455666f73f8d202712
@snowman <jimmoffitt@yahoo.com>
2024-11-12 18:32:04 -0700

    More tweaking of asyncio and aiohttp details
    Seems to run better ;)

--- bluesky_poster.py
+++ bluesky_poster.py
@@ -3,6 +3,7 @@ from tweet_archive_parser import TweetArchiveParser
 import requests
 import os
 import sys
+import re
 import json
 from typing import Dict, List
 from pathlib import Path
@@ -21,32 +22,7 @@ class BlueskyPoster:
         self.session_lock = asyncio.Lock()
         self.session = None
         self.session_expiry = None
-
-    def bsky_login_sessionX(self, pds_url: str, handle: str, password: str) -> Dict:
-
-        headers = {'Content-Type': 'application/json'}
-
-        try:
-            resp = requests.post(
-                pds_url + "/xrpc/com.atproto.server.createSession",
-                json={"identifier": handle, "password": password},
-                headers = headers
-            )
-            print(resp.json())  
-            resp.raise_for_status()
-            return resp.json()
-        except requests.exceptions.HTTPError as e:
-            print(f"HTTP error occurred: {e}")
-            if resp.status_code == 401:
-                print("Authentication failed. Please check your handle and password.")
-            return None
-        except requests.exceptions.RequestException as e:
-            print(f"An error occurred during the request: {e}")
-            return None 
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            return None
-
+    
     async def bsky_login_session(self, pds_url: str, handle: str, password: str) -> Dict:  # Make bsky_login_session async
 
         headers = {'Content-Type': 'application/json'}
@@ -79,24 +55,12 @@ class BlueskyPoster:
 
             return self.session
 
-    def upload_image(self, config, image_path):
+    async def upload_image(self, config, image_path):
         """
         Uploads an image to a Bluesky server using com.atproto.repo.uploadBlob.
-
-        Args:
-            args: An object containing the following attributes:
-                image: The path to the image file.
-                pds_url: The base URL of the server.
-                accessJwt: The access token for authentication.
-
-        Returns:
-            A dictionary containing the 'cid' and 'mimeType' of the uploaded image 
-            if successful, or None if there is an error.
         """
 
-        # Build image_path
         image_path = f"{config['images_folder']}/{image_path}"
-
 
         if not os.path.exists(image_path):
             print(f"File does not exist: {image_path}")
@@ -106,41 +70,66 @@ class BlueskyPoster:
             with open(image_path, "rb") as image_file:
                 img_bytes = image_file.read()
 
-            # this size limit is specified in the app.bsky.embed.images lexicon
             if len(img_bytes) > 1000000:
                 raise Exception(
                     f"image file size too large. 1000000 bytes maximum, got: {len(img_bytes)}"
                 )
-            
-            #TODO: strip EXIF metadata here, if needed
 
-            resp = requests.post(
-                config['pds_url'] + "/xrpc/com.atproto.repo.uploadBlob",
-                headers={
-                    "Content-Type": "image/png",
-                    "Authorization": "Bearer " + config['accessJwt']
-                },
-                data=img_bytes,
-            )
-
-            resp.raise_for_status()
-            blob = resp.json()["blob"]
+            async with aiohttp.ClientSession() as session:
+                resp = await session.post(
+                    config['pds_url'] + "/xrpc/com.atproto.repo.uploadBlob",
+                    headers={
+                        "Content-Type": "image/png",
+                        "Authorization": "Bearer " + config['accessJwt']
+                    },
+                    data=img_bytes,
+                )
+                resp.raise_for_status()
+                blob = (await resp.json())["blob"] 
 
             return blob
 
-        except requests.exceptions.RequestException as e:
+        except aiohttp.ClientError as e:  # Catch aiohttp exceptions
             print(f"Error uploading image: {e}")
             return None
 
-    def upload_file(self, pds_url: str, access_token: str, filename: str, file_data: bytes) -> Dict:
+    async def upload_file(self, pds_url: str, access_token: str, filename: str, file_data: bytes) -> Dict:
         headers = {"Authorization": f"Bearer {access_token}"}
         url = f"{pds_url}/xrpc/com.atproto.repo.uploadBlob"
-        files = {"file": (filename, file_data, "image/jpeg")}
         
-        response = requests.post(url, headers=headers, files=files)
-        response.raise_for_status()
-        return response.json()
+        async with aiohttp.ClientSession() as session:
+            async with aiohttp.MultipartWriter('form-data') as mpwriter:
+                part = mpwriter.append(file_data, {'Content-Type': 'image/jpeg'})
+                part.set_content_disposition('form-data', name='file', filename=filename)
 
+            resp = await session.post(url, headers=headers, data=mpwriter)
+            resp.raise_for_status()
+            return await resp.json()
+  
+    def manage_bluesky_message_length(self, tweet):
+        """
+        Manages the length of a Bluesky message to keep it under 300 characters.
+
+        Args:
+            tweet: A dictionary representing a tweet, containing a 'text' key.
+
+        Returns:
+            A string with the processed tweet and addendum, ready for Bluesky.
+        """
+
+        # Remove the URL from the tweet
+        tweet['text'] = re.sub(r"https://t\.co/\S+", "", tweet['text']).strip()
+
+        # Create the long and short versions of the addendum
+        long_addendum = f"\n\nTweeted at {tweet['timestamp']} UTC"
+        short_addendum = f"\nTweeted {tweet['timestamp'].split()[0]}"
+
+        # Check if the long addendum fits within the character limit
+        if len(tweet['text'] + long_addendum) <= 300:
+            return tweet['text'] + long_addendum
+        else:
+            return tweet['text'] + short_addendum
+    
     async def create_post(self, config, tweet):
         bsky_session = await self.get_or_create_session()
         if bsky_session is None:
@@ -149,18 +138,21 @@ class BlueskyPoster:
         config['accessJwt'] = bsky_session["accessJwt"]
         config['did'] = bsky_session["did"]
 
+        tweet['text' ]= self.manage_bluesky_message_length(tweet)
+
         # trailing "Z" is preferred over "+00:00"
         now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
-        addendum = f"\n\nOriginally Tweeted at {tweet['timestamp']} UTC"
 
         # Use the parse_facets function to generate facets
-        facets = parse_facets(tweet['text'] + addendum, self.pds_url)
+        #facets = parse_facets(tweet['text'] + addendum, self.pds_url)
+        facets = parse_facets(tweet['text'], self.pds_url)
 
         # these are the required fields which every post must include
         post = {
             # TODO: make general with pds_url
             "$type": "app.bsky.feed.post",
-            "text": tweet['text'] + addendum,
+            #"text": tweet['text'] + addendum,
+            "text": tweet['text'],
             "createdAt": now,
             "facets": facets,
         }
@@ -195,61 +187,6 @@ class BlueskyPoster:
             print("createRecord response:", file=sys.stderr)
             print(json.dumps(await resp.json(), indent=2))  # Use await for async json response
             resp.raise_for_status()
-
-    async def create_postX(self, config, tweet):
-        session = await self.get_or_create_session()
-        if session is None:
-            return
-
-        config['accessJwt'] = session["accessJwt"]
-        config['did'] = session["did"]
-
-        # trailing "Z" is preferred over "+00:00"
-        now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
-        addendum = f"\n\nOriginally Tweeted at {tweet['timestamp']} UTC"
-
-        # Use the parse_facets function to generate facets
-        facets = parse_facets(tweet['text'] + addendum, self.pds_url)
-
-        # these are the required fields which every post must include
-        post = {
-            # TODO: make general with pds_url
-            "$type": "app.bsky.feed.post",
-            "text": tweet['text'] + addendum,
-            "createdAt": now,
-            "facets": facets,
-        }
-
-        if tweet['image_paths']:
-            image_embed = {  # Initialize the embed dictionary outside the loop
-                # TODO: make general with pds_url
-                "$type": "app.bsky.embed.images",
-                "images": []
-            }
-            for image_path in tweet['image_paths']:
-                blob = self.upload_image(config, image_path)
-                if blob:
-                    image_setting = {
-                        "alt": '',
-                        "image": blob
-                    }
-                    image_embed["images"].append(image_setting)  # Append each image to the "images" list
-
-            post["embed"] = image_embed  # Assign the embed after processing all images
-
-
-        resp = requests.post(
-            config['pds_url'] + "/xrpc/com.atproto.repo.createRecord",
-            headers={"Authorization": "Bearer " + session["accessJwt"]},
-            json={
-                "repo": session["did"],
-                "collection": "app.bsky.feed.post",
-                "record": post,
-            },
-        )
-        print("createRecord response:", file=sys.stderr)
-        print(json.dumps(resp.json(), indent=2))
-        resp.raise_for_status()
         
 async def main():
 
@@ -292,7 +229,7 @@ async def main():
     tweets = []
     tweets = twitter_parser.extract_metadata(tweets_raw)
 
-    # TODO: need a Tweet to post. 
+    # TODO: need a Tweet to post. Pick one to test with. 
     #tweet = tweets[188]
     # tweet_id = '1662845607046795264' OK
     # tweet_id = '1217911688827211777' OK
